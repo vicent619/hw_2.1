64cf179a5c72e8823777377369ec6dfe9b496767
void33x <w2929997@gmail.com>
2026-01-30 10:34:04 +0200

    Add miles to kilometers conversion

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,19 @@
 package app;
 
 public class Main {
+    private static final double CONV_K = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("Project: Converter");
         System.out.println("Converts miles to kilometers and kilometers to miles");
+
+        double miles = 10;
+        double kilometers = convMilesToKilometers(miles);
+
+        System.out.println(miles + " miles = " + kilometers + " kilometers");
+    }
+
+    private static double convMilesToKilometers(double miles) {
+        return miles * CONV_K;
     }
 }
