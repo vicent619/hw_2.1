491e926649e5bd3d2a68be20dc3eabad575a45ce
void33x <w2929997@gmail.com>
2026-01-30 10:26:15 +0200

    Initial commit: Create Converter project and project info output

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Project: Converter");
+        System.out.println("Converts miles to kilometers and kilometers to miles");
+    }
+}
